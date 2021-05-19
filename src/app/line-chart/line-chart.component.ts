5267e07384efe85e6fcadc4d0697dcb62f58a1fc
MikeYef96 <mike.yef@gmail.com>
2021-05-19 15:13:56 +0300

    fixed axis-y line, bug with changing charts,  now it's static

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { ChartConfiguration, ChartEvent, ChartType } from "chart.js";
 import { BaseChartDirective } from "ng2-charts";
-import * as database from "../../../db.json";
-
-const DATA_COUNT = 5;
-const NUMBER_CFG = { count: DATA_COUNT, min: -80, max: 100 };
 
 @Component({
   selector: "app-line-chart",
@@ -15,7 +11,7 @@ export class LineChartComponent {
   public lineChartData: ChartConfiguration["data"] = {
     datasets: [
       {
-        data: [-44.69, 5.03, 13.32, 40.23, 43.22],
+        data: [-44.69, -13.21, -4.47, 40.23, 80.12],
         label: "1 Previous Orders",
         backgroundColor: "rgba(40, 78, 245,1)",
         pointBackgroundColor: "rgba(40, 78, 245,0)",
@@ -37,9 +33,8 @@ export class LineChartComponent {
         fill: "origin",
       },
       {
-        data: [-16.52, -3, 2.73, 9.28, 20.89],
+        data: [-16.52, -1, 5.17, 13.32, 20.89],
         label: "3 Previous Orders",
-        yAxisID: "y-axis-1",
         backgroundColor: "rgba(231, 151, 31, 1)",
         borderColor: "rgba(42, 160, 7, 0)",
         pointBackgroundColor: "rgba(40, 78, 245,0)",
@@ -51,7 +46,6 @@ export class LineChartComponent {
       {
         data: [-1.82, 7.06, 11.28, 10.39, 16.79],
         label: "4-12 Previous Orders",
-        yAxisID: "y-axis-1",
         backgroundColor: "rgba(42, 160, 7, 1)",
         pointBackgroundColor: "rgba(40, 78, 245,0)",
         borderColor: "rgba(42, 160, 7, 0)",
@@ -80,11 +74,13 @@ export class LineChartComponent {
         },
       },
       y: {
-        stacked: true,
+        stacked: false,
         title: {
           display: true,
           text: "Value",
         },
+        min: -80,
+        max: 100,
       },
     },
 
@@ -106,21 +102,6 @@ export class LineChartComponent {
   public lineChartType: ChartType = "line";
 
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
-
-  private static generateNumber(i: number): number {
-    return Math.floor(Math.random() * (i < 2 ? 100 : 1000) + 1);
-  }
-
-  public randomize(): void {
-    for (let i = 0; i < this.lineChartData.datasets.length; i++) {
-      for (let j = 0; j < this.lineChartData.datasets[i].data.length; j++) {
-        this.lineChartData.datasets[i].data[
-          j
-        ] = LineChartComponent.generateNumber(i);
-      }
-    }
-    this.chart?.update();
-  }
 
   // events
   public chartClicked({
@@ -146,32 +127,5 @@ export class LineChartComponent {
   public hideOne(): void {
     const isHidden = this.chart?.isDatasetHidden(1);
     this.chart?.hideDataset(1, !isHidden);
-  }
-
-  public pushOne(): void {
-    this.lineChartData.datasets.forEach((x, i) => {
-      const num = LineChartComponent.generateNumber(i);
-      x.data.push(num);
-    });
-    this.lineChartData?.labels?.push(
-      `Label ${this.lineChartData.labels.length}`
-    );
-
-    this.chart?.update();
-  }
-
-  public changeColor(): void {
-    this.lineChartData.datasets[2].borderColor = "green";
-    this.lineChartData.datasets[2].backgroundColor = `rgba(0, 255, 0, 0.3)`;
-
-    this.chart?.update();
-  }
-
-  public changeLabel(): void {
-    if (this.lineChartData.labels) {
-      this.lineChartData.labels[2] = ["1st Line", "2nd Line"];
-    }
-
-    this.chart?.update();
   }
 }
